9c77cc67bc09d98aa01bf22901f5dbd169665ab1
b-bots-dev <b.bots.dev@gmail.com>
2023-10-14 10:25:31 +0200

    initial commit - empty bot structure

--- TELEGRAM/BotMain.py
+++ TELEGRAM/BotMain.py
@@ -4,13 +4,13 @@ from pyrogram import Client, idle
 
 def start_telegram_bot():
 
-    print('BOT STARTING...')
+    print("BOT STARTING...")
 
     bot = Client("DATA\MULTIPLATFORM",
-                 api_id=os.environ['API_ID'],
-                 api_hash=os.environ['API_HASH'],
-                 bot_token=os.environ['BOT_TOKEN'],
-                 plugins=dict(root='TELEGRAM\handlers'),
+                 api_id=os.environ["API_ID"],
+                 api_hash=os.environ["API_HASH"],
+                 bot_token=os.environ["BOT_TOKEN"],
+                 plugins=dict(root="TELEGRAM\handlers"),
                  max_concurrent_transmissions=4)
 
     try:
@@ -19,6 +19,6 @@ def start_telegram_bot():
         pass
 
     bot.start()
-    print('BOT STARTED')
+    print("BOT STARTED")
     idle()
     bot.stop()
