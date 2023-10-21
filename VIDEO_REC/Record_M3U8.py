c54cbcda454520da1b6204e0be82da4e65dfd183
b-bots-dev <b.bots.dev@gmail.com>
2023-10-21 18:57:26 +0200

    record pipe, connect test, base text handler / todo : video folders

--- VIDEO_REC/Record_M3U8.py
+++ VIDEO_REC/Record_M3U8.py
@@ -12,7 +12,9 @@ class ReadStderr(Thread):
         self.line = None
         self.last_line = None
         self.penultimate_line = None
-        self.name = f'RecReader : {fn}'
+        self.record_time = None
+        self.error = None
+        self.name = f'M3U8.RecReader : {fn}'
 
     def run(self):
         self.line = self.process.stderr.readline()
@@ -28,8 +30,30 @@ class ReadStderr(Thread):
                 del rec_procs[self.fn]
                 break
 
+        # PROCESS FINISHED
         else:
-            # finished
+            self.last_line = self.last_line.decode('utf-8')
+            self.penultimate_line = self.penultimate_line.decode('utf-8')
+            # print(self.penultimate_line)
+            # print(self.last_line)
+
+            # RECORD FINISHED
+            if 'time' in self.penultimate_line:
+                _parts = self.penultimate_line.split()
+                for _part in _parts:
+                    if 'time' in _part:
+                        self.record_time = _part.split('=')[1]
+
+                t_parts = self.record_time.split(':')
+                h = t_parts[0]
+                m = t_parts[1]
+                s = round(float(t_parts[2]))
+                self.record_time = f'{h}:{m}:{s}'
+
+            # ERROR 404
+            if '404 Not Found' in self.last_line or '404 Not Found' in self.penultimate_line:
+                self.error = '404 Not Found'
+
             time.sleep(3)
             self.process.terminate()
             del rec_procs[self.fn]
@@ -46,7 +70,8 @@ class RecordM3U8:
         self.process = (
             ffmpeg
             .input(self.url)
-            .output(filename=self.fn, codec='copy', t='15')
+            .output(filename=self.fn, codec='copy', t='10')
+            .overwrite_output()
         )
 
         self.process = self.process.run_async(pipe_stdin=True, pipe_stderr=True)
